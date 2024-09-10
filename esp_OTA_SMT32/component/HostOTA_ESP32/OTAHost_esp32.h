3f69633cb8d3e086121d8afbdc4f68be776f2d10
phusuper <phusuper68@gmail.com>
2024-09-10 07:40:46 +0700

    update

--- esp_OTA_SMT32/component/HostOTA_ESP32/OTAHost_esp32.h
+++ esp_OTA_SMT32/component/HostOTA_ESP32/OTAHost_esp32.h
@@ -66,13 +66,56 @@ typedef struct
 
 // Function prototypes
 /**
- * @brief Init protocol MIN
- * @param min_ctx MIN device descriptor
+ * @brief Initializes the MIN protocol.
+ * 
+ * This function initializes the MIN protocol for ESP32, allowing communication between ESP32 and STM32.
+ * 
+ * @param min_ctx Pointer to the `min_context` structure used to manage the MIN communication.
  */
 void initMin(struct min_context *min_ctx);
+
+/**
+ * @brief Sends OTA data with a specified type.
+ * 
+ * This function sends a data frame containing an operation code with a specified command type (data_type)
+ * to STM32.
+ * 
+ * @param data_type Type of OTA data to send (e.g., START, HEADER, DATA, END).
+ * @param operation_code The operation code or data to be sent within the frame.
+ * @param length The length of the data in the `operation_code`.
+ */
 void ota_send_data(OTA_Data_Type data_type, uint8_t *operation_code, uint8_t length);
-void ota_send_start_end(OTA_Data_Type start_or_end_command_type,uint8_t length_one_command);
+
+/**
+ * @brief Sends START or END command for the OTA process.
+ * 
+ * This function sends the start or end command for the OTA process. The START command is sent before
+ * transmitting data, and the END command is sent after all data has been transmitted.
+ * 
+ * @param start_or_end_command_type The type of command (START or END) to be sent.
+ * @param length_one_command The length of the command to be sent, typically the number of bytes in a command frame.
+ */
+void ota_send_start_end(OTA_Data_Type start_or_end_command_type, uint8_t length_one_command);
+
+/**
+ * @brief Handles the response from STM32.
+ * 
+ * This function reads and processes the response from STM32 over UART. It checks the type of response
+ * received (ACK, NACK, etc.) to determine whether the data transmission was successful.
+ */
 void response_command_of_STM32_handle();
-void otaStart(char *file_hex,bool *status_kick_off);
+
+/**
+ * @brief Starts the OTA process using hex file data.
+ * 
+ * This function initiates the OTA process by parsing and sending data from a hex file. The hex file
+ * is divided into smaller data lines, each of which is sent sequentially to STM32. The process includes
+ * sending START, HEADER, DATA, and END commands.
+ * 
+ * @param file_hex String containing the hex file data.
+ * @param status_kick_off Pointer to a flag to track whether the OTA process has started.
+ */
+void otaStart(char *file_hex, bool *status_kick_off);
+
 
 #endif // HOST_BOOTLOADER_H
